431a7f5fb174dc44676b6de08be8da9f4f0610e0
Ariton Sefedini <aritonsefedini@icloud.com>
2021-02-25 16:32:29 -0500

    Tried our best

--- skeleton-9/frontend/follow_toggle.js
+++ skeleton-9/frontend/follow_toggle.js
@@ -1,12 +1,15 @@
 class FollowToggle {
-    $followBtn = $('.follow-toggle')
-    constructor() {
-        this.userID = $('.follow-toggle').data('user-id');
-        this.followState = $('.follow-toggle').data('initial-follow-state');
+    $followBtn = $('button.follow-toggle')
+    constructor(btn) {
+        this.$btn = $(btn)
+        this.userID = this.$followBtn.data('user-id');
+        this.followState = this.$followBtn.data('initial-follow-state');
         this.render();
+
+        this.$btn.on('click', this.handleClick.bind(this));
     };
 
-    render() {
+    render(btn) {
         if (this.followState === 'unfollowed') {
             this.$followBtn.text('Follow')
         } else if (this.followState === 'followed') {
--- skeleton-9/frontend/twitter.js
+++ skeleton-9/frontend/twitter.js
@@ -1,5 +1,5 @@
 const FollowToggle = require('./follow_toggle.js');
 
 $(() => {
-   
+   $('button.follow-toggle').each( (i, btn) => new FollowToggle(btn, {}) );
 })
